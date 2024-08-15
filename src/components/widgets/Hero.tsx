d6954aef01ab8b9c2b3bcc95008b31d3598f257e
KrrishSodho <kirshansinghrajput@gmail.com>
2024-08-16 01:03:01 +0500

    responsive header

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -15,17 +15,31 @@ import custemer6 from "@/assets/customers/customer-6.jpg";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col mt-0  gap-2 items-center  bg-gray-100 rounded-[2.5rem] m-6 lg:flex-row lg:pl-8 ">
-      <div className=" flex flex-col justify-center items-center lg:basis-[55%]  lg:pl-4  ">
-        <div className="flex flex-col items-center justify-center">
+    <div
+      className="
+    flex m-6  mt-0  gap-2  bg-gray-100 rounded-[2.5rem]   
+    max-sm:flex-col max-sm:items-center max-sm:p-2
+    max-md:flex-col 
+    max-lg:items-center
+    lg:pl-8 lg:items-center lg:justify-center
+    "
+    >
+      <div className=" flex flex-col max-lg:basis-[60%] lg:basis-[60%]    ">
+        <div className="flex flex-col ">
           {/* Left Side */}
-          <div className="text-center font-semibold p-6 text-3xl lg:font-bold lg:text-5xl text-[#333]">
+          <div className="text-center max-sm:p-2 font-bold max-md:px-4 max-sm:text-xl max-md:text-2xl max-lg:text-4xl  lg:text-left lg:ml-0 lg:p-0 lg:text-5xl text-[#333]">
             <h1>
               Certified Cloud Native Applied Generative AI Engineer - PIAIC
             </h1>
           </div>
-          <div className="flex text-center justify-between  items-center  ">
-            <p className="font-medium px-2 ">
+          <div
+            className="flex   
+          text-center justify-center lg:pt-8
+          sm:text-center sm:justify-between sm:p-4 
+          lg:text-start  lg:px-0
+          "
+          >
+            <p className="font-medium max-sm:text-sm max-md:text-base lg:text-lg  ">
               The Cloud Native Applied Generative AI Certification program
               equips you to create leading-edge Cloud Native AI and Physical AI
               solutions using a comprehensive cloud-native, AI, and Physical AI
@@ -34,14 +48,18 @@ const Hero = () => {
           </div>
         </div>
 
-        <div className="flex items-center space-x-3 justify-center mt-8 lg:mt-14 lg:items-center  ">
-          <Buttons name="Apply Now" />
-          <button className="  font-bold rounded-[9px] py-2 px-4 lg:py-3 lg:px-8 bg-white hover:bg-gray-100 hover:outline hover:outline-lime-600">
-            {" "}
-            Learn More &#8595;
-          </button>
+        <div className="flex space-x-3 max-lg:items-center max-lg:justify-center  max-lg:mt-4 lg:mt-8 lg:items-start ">
+          <div>
+            <Buttons name="Apply Now" />
+          </div>
+          <div>
+            <button className="  font-bold rounded-[9px] py-2 px-4 lg:py-3 lg:px-8 bg-white hover:bg-gray-100 hover:outline hover:outline-lime-600">
+              {" "}
+              Learn More &#8595;
+            </button>
+          </div>
         </div>
-        <div className=" flex items-center justify-center space-x-6 mt-8 lg:mt-14">
+        <div className=" flex items-center max-lg:justify-center space-x-6 max-lg:mb-2 max-lg:mt-6 max-lg:px-2 lg:mt-8 lg:pb-4 ">
           <div className="flex items-center ">
             <Image className="customer" src={custemer1} alt="custemer 1" />
             <Image className="customer" src={custemer2} alt="custemer 2" />
@@ -51,16 +69,14 @@ const Hero = () => {
             <Image className="customer" src={custemer6} alt="custemer 6" />
           </div>
           <div>
-            <p className=" text-[12px] ">
-              <span className="text-red-700 font-bold lg:text-lg ">
-                10,000+
-              </span>
+            <p className=" text-xs sm:text-sm md:text-base lg:text-lg">
+              <span className="text-red-700 font-bold  ">10,000+</span>
               tech community!
             </p>
           </div>
         </div>
       </div>
-      <div className=" flex items-end p-4 justify-center">
+      <div className=" flex items-end p-4 justify-center max-lg:w-[50%] max-lg:h-[50%]">
         <Image className="" src={Hero5} alt="Hero Poster Student" />
       </div>
     </div>
